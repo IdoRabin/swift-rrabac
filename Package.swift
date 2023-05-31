4b902e66c4106bff9244502b3d9053043dab2544
Ido Rabin <ido@sync.me>
2023-05-31 13:35:51 +0300

    initial commit

--- Package.swift
+++ Package.swift
@@ -12,15 +12,21 @@ let package = Package(
             targets: ["RRABAC"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        // In-House pakcages
+       .package(path: "../../xcode/DSLogger"),
+       .package(path: "../../xcode/MNUtils/MNUtils"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RRABAC",
-            dependencies: []),
+            dependencies: [
+                // In-House pakcages
+                .product(name: "DSLogger", package: "DSLogger"),
+                .product(name: "MNUtils", package: "MNUtils")
+            ]
+        ),
         .testTarget(
             name: "RRABACTests",
             dependencies: ["RRABAC"]),
