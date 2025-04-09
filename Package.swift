d7bcbc339b47884f106b868cbfa6c77ee799d092
Ido Rabin <idorabin@gmail.com>
2025-04-09 20:57:09 +0300

    minor package file change

--- Package.swift
+++ Package.swift
@@ -6,22 +6,23 @@ import PackageDescription
 let package = Package(
     name: "RRabac",
     platforms: [
-        .macOS(.v13)
+        .macOS(.v13),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "RRabac",
-            targets: ["RRabac"]),
+            targets: ["RRabac"]
+        ),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.76.0"),
-        .package(url: "https://github.com/vapor/fluent.git", from: "4.8.0"),
-        
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.106.0"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.12.0"),
+
         /// In-House pakcages
         // .package(url: "https://gitlab.com/ido_r_demos/dslogger.git", from: "0.0.1"),
         // .package(url: "https://gitlab.com/ido_r_demos/mnutils.git", from: "0.0.2"),
-        .package(path:"../MNVaporUtils/")
+        .package(path: "../MNVaporUtils/"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -33,7 +34,7 @@ let package = Package(
                 // .product(name: "DSLogger", package: "DSLogger"),
                 // .product(name: "MNUtils", package: "MNUtils"),
                 .product(name: "MNVaporUtils", package: "MNVaporUtils"),
-                
+
                 .product(name: "Fluent", package: "fluent"),
                 .product(name: "Vapor", package: "vapor"),
             ],
@@ -44,6 +45,7 @@ let package = Package(
         ),
         .testTarget(
             name: "RRabacTests",
-            dependencies: ["RRabac"]),
+            dependencies: ["RRabac"]
+        ),
     ]
 )
